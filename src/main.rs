e9695dc926263e7ca32c771e65def978fd3d2027
Bunny Bites <s.mukeshkr481@gmail.com>
2024-01-16 14:12:15 +0530

    testing local setup for solana

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::{commitment_config::CommitmentConfig, native_token::LAMPORTS_PER_SOL, signature::Keypair, signer::Signer};
+
 fn main() {
-    println!("Hello, world!");
+    let wallet = Keypair::new();
+    let pubkey = Signer::pubkey(&wallet);
+    let rpc_url = String::from("http://127.0.0.1:8899");
+    let client = RpcClient::new_with_commitment(rpc_url, CommitmentConfig::confirmed());
+
+    match client.request_airdrop(&pubkey, LAMPORTS_PER_SOL) {
+        Ok(sig) => loop {
+            if let Ok(confirmed) = client.confirm_transaction(&sig) {
+                if confirmed {
+                    println!("Transaction: {} Status: {}", sig, confirmed);
+                    break;
+                }
+            }
+        },
+        Err(_) => println!("Error requesting airdrop"),
+    };
 }
