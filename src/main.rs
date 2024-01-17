2197f8ca334466cbbefe45b5e3577f9230cdeae2
Bunny Bites <s.mukeshkr481@gmail.com>
2024-01-17 10:38:37 +0530

    add functionality to compare balance

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ use solana_sdk::{commitment_config::CommitmentConfig, native_token::LAMPORTS_PER
 fn main() {
     let wallet = Keypair::new();
     let pubkey = Signer::pubkey(&wallet);
-    let rpc_url = String::from("http://127.0.0.1:8899");
+    let rpc_url = String::from("http://127.0.0.1:8899"); // local setup
     let client = RpcClient::new_with_commitment(rpc_url, CommitmentConfig::confirmed());
+
+    // check the available sols for our wallet (with public key) - before airdroping
+    match client.get_balance(&pubkey) {
+        Ok(balance) => {
+            println!("{}", balance);
+        },
+        Err(_) => println!("Error getting balance"),
+    };
 
     // get sols (this are used for transactions)
     match client.request_airdrop(&pubkey, LAMPORTS_PER_SOL) {
@@ -20,12 +28,11 @@ fn main() {
         Err(_) => println!("Error requesting airdrop"),
     };
 
-    // check the available sols for our wallet (with public key)
+    // check the available sols for our wallet (with public key) - after airdroping
     match client.get_balance(&pubkey) {
         Ok(balance) => {
             println!("{}", balance);
         },
         Err(_) => println!("Error getting balance"),
     };
-
 }
